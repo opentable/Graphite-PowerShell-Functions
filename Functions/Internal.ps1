ab1f55d3ebfa641769435b4d6d8523aa5aafb9b4
Maxime DEVALLAND <mdevalland@opentable.com>
2016-11-09 12:01:23 +0000

    reimporting changes after repository deletion

--- Functions/Internal.ps1
+++ Functions/Internal.ps1
@@ -38,7 +38,7 @@ Function Import-XMLConfig
 
     # Get the HostName to use for the metrics from the config file
     $Config.NodeHostName = $xmlfile.Configuration.Graphite.NodeHostName
-    
+
     # Set the NodeHostName to ComputerName
     if($Config.NodeHostName -eq '$env:COMPUTERNAME')
     {
@@ -110,8 +110,8 @@ Function Import-XMLConfig
         [int]$Config.MSSQLQueryTimeout = $xmlfile.Configuration.MSSQLMetics.SQLQueryTimeoutSeconds
 
         # Create the Performance Counters Array
-        $Config.MSSQLServers = @()     
-     
+        $Config.MSSQLServers = @()
+
         foreach ($sqlServer in $xmlfile.Configuration.MSSQLMetics)
         {
             # Load each SQL Server into an array
